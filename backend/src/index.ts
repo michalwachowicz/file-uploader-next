b0c9807f7863de45a5450723e586a02a267ebdb8
michalwachowicz <wachowiczmichal1@gmail.com>
2026-01-07 15:45:56 +0100

    feat(be): configure cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { config } from "@/lib/config";
 import apiRouter from "@/routes/api";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", apiRouter);
