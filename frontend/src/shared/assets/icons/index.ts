5880ca5603714c724df6bd43ddf581a3ef976486
michalwachowicz <wachowiczmichal1@gmail.com>
2026-01-15 12:56:35 +0100

    feat(fe): add shared icons

---
+++ frontend/src/shared/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as ArrowDropdownIcon } from "./arrow-dropdown.svg";
+export { default as AddIcon } from "./add.svg";
+export { default as CloseIcon } from "./close.svg";
+export { default as FileIcon } from "./file.svg";
+export { default as FolderIcon } from "./folder.svg";
+export { default as FolderSharedIcon } from "./folder-shared.svg";
